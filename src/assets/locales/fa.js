c95f3841344466dbcac1234850ba73562d57c613
majidln <majidlotfinia12@gmail.com>
2020-09-01 15:28:01 +0430

    add fa language

---
+++ src/assets/locales/fa.js
@@ -0,0 +1,12 @@
+export default {
+  greeting: 'سلام',
+  floatingMenu: {
+    income: 'ورودی',
+    expense: 'خروجی'
+  },
+  navigation: {
+    home: 'خانه',
+    setting: 'تنظیمات',
+    income: 'ورودی',
+  }
+};
