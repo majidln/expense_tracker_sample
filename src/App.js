8b713e1422aaebfa14dd9d4e5e657de107118c8c
majidln <majidlotfinia12@gmail.com>
2020-09-01 12:53:33 +0430

    add theme providers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import MainStackNavigator from './navigations'
+import { ThemeContextProvider } from './providers/ThemeProviders';
 
 export default function App() {
-  return <MainStackNavigator />
+  return (
+    <ThemeContextProvider>
+      <MainStackNavigator />
+    </ThemeContextProvider>
+  )
 }
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { withTheme } from './../providers/ThemeProviders';
 
-function MainScreen() {
+function MainScreen({theme}) {
   return (
-    <View style={styles.container}>
-      <Text>Main Screen</Text>
+    <View style={[styles.container, {backgroundColor: theme.backgroundColor}]}>
+      <Text styles={{color: theme.primaryText}}>Main Screen</Text>
     </View>
   )
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
   }
 })
 
-export default MainScreen;
+export default withTheme(MainScreen);
